6c10c270478fd63da7a6e855baa12c3aae237966
svvqt <kon.vitkovskii@gmail.com>
2023-03-28 22:28:03 +0600

    finally, final ver (mb 1.1)

--- Parse.py
+++ Parse.py
@@ -1,34 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
-import transliterate
+import transliterate 
 def parser():
-    url = 'https://auto.drom.ru/'
-    page = requests.get(url)
-    print(page.status_code)
-    rus="А Б В Г Д Е Ё Ж З И Й К Л М Н О П Р С Т У Ф Х Ц Ч Ш Щ Ъ Ы Ь Э Ю Я"
-    price=[]
-    pricefiltered=[]
-    newcars=[]
-    newcarsfiltered=[]
-    soup = BeautifulSoup(page.text, "html.parser")
-    newcars = soup.findAll('div',class_='css-13ocj84 e1icyw250')
-    price=soup.findAll('span',class_='css-46itwz e162wx9x0')
-    for data in newcars:
-        if data.find('span') is not None:
-            newcarsfiltered.append(data.text)
+    url = 'https://auto.drom.ru/' # передаем необходимы URL адрес
+    page = requests.get(url) # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
+    print(page.status_code) # смотрим ответ
+    rus="А Б В Г Д Е Ё Ж З И Й К Л М Н О П Р С Т У Ф Х Ц Ч Ш Щ Ъ Ы Ь Э Ю Я" # Русский алфавит, нужен для транслита
+    price=[] # цена со странички
+    pricefiltered=[] # отфильтрованная цена
+    newcars=[] # Данные машин со странички
+    newcarsfiltered=[] # отфильтрованные данные машин
+    soup = BeautifulSoup(page.text, "html.parser") # передаем страницу в bs4
+    newcars = soup.findAll('div',class_='css-13ocj84 e1icyw250') # записываем нужный класс
+    price=soup.findAll('span',class_='css-46itwz e162wx9x0') # записываем нужный класс
+    for data in newcars: # проходим циклом по содержимому контейнера
+        if data.find('span') is not None: # находим тег "span"
+            newcarsfiltered.append(data.text) # записываем в переменную содержание тега
     for data in price:
         if data.find('span') is not None:
             pricefiltered.append(data.text)
     for i in range(20):
-        a=newcarsfiltered[i]
-        b=str(a.split()[0])
-        bukva=b.split()[0]
+        a=newcarsfiltered[i] # записываем в переменную данные
+        bukva=str(a.split(",")[0]) # первая буква
         for j in range(33):
-            if (rus.split(" ")[j] == bukva[0]):
-                a=str(newcarsfiltered[i])
-                newcarsfiltered[i]=transliterate.translit(a, reversed=True)
-        print(newcarsfiltered[i].split(",")[0]+', '+pricefiltered[i])
+            if (rus.split(" ")[j] == bukva[0]): # если буква в русском алфавите == первой букве в названии
+                newcarsfiltered[i]=transliterate.translit(a, reversed=True) # с помощью библиотеки русское название переводится на латиницу
+        print(newcarsfiltered[i].split(",")[0]+', '+pricefiltered[i]) # выводим название+цену
     with open("spisok.txt", "w",encoding="utf-8") as file:
         for i in range(20):
-            file.write(newcarsfiltered[i].split(",")[0]+', '+pricefiltered[i])
+            file.write(newcarsfiltered[i].split(",")[0]+', '+pricefiltered[i]) # записываем в файл название+цену
             file.write('\n')
